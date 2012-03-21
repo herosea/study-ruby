da39b9875684a0bb6cccf6a2e18f86562d78e678
HeroSea <herosea.zhou@gmail.com>
2012-03-21 22:00:33 +0800

    add rails app of map

---
+++ map.herosea.me/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MapHeroseaMe::Application.config.secret_token = '9712f0346ee393789b7a899f25b409a8f642afbda9c9f1c2748eb78dbafa2d1a9fc6da91e7f6dd10fdf1f8ffc2104422fe1e3f14e765fe1c71ff719bab6619a0'
